b084a56a19af153e1b0666f47d496ef2cc739cf4
TejasPatne <tejaspatne18@gmail.com>
2024-01-14 18:09:15 +0530

    add CTA section

--- components/Button.tsx
+++ components/Button.tsx
@@ -5,16 +5,17 @@ type ButtonProps = {
     title: string;
     icon?: string;
     variant: 'btn_white' | 'btn_white_text' | 'btn_green' | 'btn_dark_green' | 'btn_dark_green_outline';
+    full?: boolean;
 }
 
-export default function Button({ type, title, icon, variant }: ButtonProps) {
+export default function Button({ type, title, icon, variant, full }: ButtonProps) {
   return (
-    <button type={type} className={`flexCenter gap-3 rounded-full border ${variant}`}>
+    <button type={type} className={`flexCenter gap-3 rounded-full border ${variant} ${full && 'w-full'}`}>
         { 
             icon && 
             <Image src={icon} alt={title} width={24} height={24} />
         }
-        <label>{title}</label>
+        <label className="bold-16 whitespace-nowrap cursor-pointer">{title}</label>
     </button>
   )
 }
